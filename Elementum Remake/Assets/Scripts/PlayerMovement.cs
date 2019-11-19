7a60ba98deaf2cf5126ed308e633cad5e09b72ad
ShiroKatake <tgkhang262@gmail.com>
2019-11-19 22:42:47 +1100

    Made wall grab and wall jump
    - Getting accidentally caught in a wall grab feels a bit rigid. Changing from a wall grab to a wall slide might make it feel a bit more natural

--- Elementum Remake/Assets/Scripts/PlayerMovement.cs
+++ Elementum Remake/Assets/Scripts/PlayerMovement.cs
@@ -1,41 +1,92 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
 	public float jumpForce;				//Player's jump force
-	public float speed = 10f;			//Player's speed
+	public float speed = 10f;           //Player's speed
+	public float wallJumpLerp;			//Determines how much movement is restricted during wall jump
 
 	private Rigidbody2D rb;             //Player's rigidbody
 	private Collision coll;				//Player's collision box
-	private Vector2 move;				//Player's movement
-	
+	private Vector2 move;               //Player's movement
+	private float initialGravity;       //Initial gravity value
+
+	private bool wallJumped;            //
+	private bool isDashing;
+	private bool wallGrab;
+	private bool canMove = true;
+
 	private void Awake() {
 		rb = GetComponent<Rigidbody2D>();
 		coll = GetComponent<Collision>();
 	}
 
+	private void Start() {
+		initialGravity = rb.gravityScale;
+	}
+
 	private void Update() {
+		//Take movement input from player
 		float x = Input.GetAxis("Horizontal");
 		float y = Input.GetAxis("Vertical");
 		Walk(new Vector2(x, y));
 
-		if(Input.GetButtonDown("Jump")) {
-			if (coll.onGround) {
-				Jump();
-			}
+		//Wall grab
+		if (coll.onWall && !coll.onGround) {
+			wallGrab = true;
+		}
+
+		if (wallGrab) {
+			canMove = false;
+			rb.velocity = Vector2.zero;
+			rb.gravityScale = 0;
+		} else {
+			rb.gravityScale = initialGravity;
+		}
+
+		if (Input.GetButtonDown("Jump")) {
+			if (coll.onGround)
+				Jump(Vector2.up, false);
+			if (wallGrab)
+				WallJump();
+		}
+
+		if(coll.onGround && !isDashing) {
+			wallJumped = false;
+			GetComponent<BetterJump>().enabled = true;
 		}
 	}
 
 	void Walk(Vector2 dir) {
-		rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
+		if (!canMove)
+			return;
+		if (!wallJumped) {
+			rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
+		} else {
+			rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * speed, rb.velocity.y)), wallJumpLerp * Time.deltaTime);
+		}
 	}
 
-	void Jump() {
+	void Jump(Vector2 dir, bool wall) {
 		//Allow for air controls
 		rb.velocity = new Vector2(rb.velocity.x, 0);
 		//Jump
-		rb.velocity += Vector2.up * jumpForce;
+		rb.velocity += dir * jumpForce;
+	}
+
+	void WallJump() {
+		StopCoroutine(DisableMovement(0));
+		StartCoroutine(DisableMovement(0.1f));
+
+		Vector2 wallDir = coll.onRightWall ? Vector2.left : Vector2.right;
+		Jump(Vector2.up / 1.5f + wallDir / 1.5f, true);
+		wallJumped = true;
+	}
+
+	IEnumerator DisableMovement(float time) {
+		wallGrab = false;
+		canMove = false;
+		yield return new WaitForSeconds(time);
+		canMove = true;
 	}
 }
