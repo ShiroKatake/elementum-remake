9dfe55235fee415b8a67e10cfc2c3a87a7dbebca
ShiroKatake <tgkhang262@gmail.com>
2019-11-23 18:27:30 +1100

    Cleaned up code indents

--- Elementum Remake/Assets/Scripts/ElementBubble.cs
+++ Elementum Remake/Assets/Scripts/ElementBubble.cs
@@ -2,33 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ElementBubble : MonoBehaviour
-{
-    public GameObject player;
-    public GameObject bubbleAnimation;
-    public GameObject runeAnimation;
-    public Element element;
+public class ElementBubble : MonoBehaviour {
+	public GameObject player;
+	public GameObject bubbleAnimation;
+	public GameObject runeAnimation;
+	public Element element;
 
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.tag == "Player")
-        {
-            if (player.GetComponent<AbilitySlot>().occupied == false && player.GetComponent<AbilitySlot>().element != element)
-            {
-                
-                player.GetComponent<AbilitySlot>().occupied = true;
-                player.GetComponent<AbilitySlot>().element = element;
-            }
-        }
-    }
+	void OnTriggerStay2D(Collider2D other) {
+		if (other.tag == "Player") {
+			if (player.GetComponent<AbilitySlot>().occupied == false && player.GetComponent<AbilitySlot>().element != element) {
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (player.GetComponent<AbilitySlot>().occupied == false)
-        {
-            bubbleAnimation.GetComponent<BubbleAnimation>().animating = true;
-            runeAnimation.GetComponent<RuneAnimation>().animating = true;
-            player.GetComponent<AbilitySlot>().element = null;
-        }
-    }
+				player.GetComponent<AbilitySlot>().occupied = true;
+				player.GetComponent<AbilitySlot>().element = element;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (player.GetComponent<AbilitySlot>().occupied == false) {
+			bubbleAnimation.GetComponent<BubbleAnimation>().animating = true;
+			runeAnimation.GetComponent<RuneAnimation>().animating = true;
+			player.GetComponent<AbilitySlot>().element = null;
+		}
+	}
 }
--- Elementum Remake/Assets/Scripts/ExitInteraction.cs
+++ Elementum Remake/Assets/Scripts/ExitInteraction.cs
@@ -3,45 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ExitInteraction : MonoBehaviour
-{
-    public int nextSceneIndex;
-    private bool playerInBounds;
+public class ExitInteraction : MonoBehaviour {
+	public int nextSceneIndex;
+	private bool playerInBounds;
 
-    void Start()
-    {
-        Color tmp = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color;
-        tmp.a = 0f;
-        transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color = tmp;
-    }
+	void Start() {
+		Color tmp = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color;
+		tmp.a = 0f;
+		transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color = tmp;
+	}
 
-    void Update()
-    {
-        if (playerInBounds)
-        {
-            Color tmp = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color;
-            tmp.a = 1f;
-            transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color = tmp;
-            if (Input.GetButtonDown("Interact"))
-            {
-                SceneManager.LoadScene(nextSceneIndex);
-            }
-        }
-        else
-        {
-            Color tmp = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color;
-            tmp.a = 0f;
-            transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color = tmp;
-        }
-    }
+	void Update() {
+		if (playerInBounds) {
+			Color tmp = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color;
+			tmp.a = 1f;
+			transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color = tmp;
+			if (Input.GetButtonDown("Interact")) {
+				SceneManager.LoadScene(nextSceneIndex);
+			}
+		} else {
+			Color tmp = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color;
+			tmp.a = 0f;
+			transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color = tmp;
+		}
+	}
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        playerInBounds = true;
-    }
+	void OnTriggerEnter2D(Collider2D other) {
+		playerInBounds = true;
+	}
 
-    void OnTriggerExit2D(Collider2D other)
-    {
-        playerInBounds = false;
-    }
+	void OnTriggerExit2D(Collider2D other) {
+		playerInBounds = false;
+	}
 }
