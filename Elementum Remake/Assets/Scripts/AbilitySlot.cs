41ef211afeb2c6f3334edbe7baf412a745a055e9
ShiroKatake <tgkhang262@gmail.com>
2019-11-23 18:30:06 +1100

    Updated AbilitySlot
    - Fixed the colors by making public color variables to change from the editor instead of entering rgb values from code

--- Elementum Remake/Assets/Scripts/AbilitySlot.cs
+++ Elementum Remake/Assets/Scripts/AbilitySlot.cs
@@ -1,40 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class AbilitySlot : MonoBehaviour
-{
-    public bool occupied;
-    public Element element;
+public class AbilitySlot : MonoBehaviour {
+	public bool occupied;
+	public Element element;
+	public Color airColor;
+	public Color fireColor;
+	public Color earthColor;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!occupied)
-        {
-            transform.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0);
-        }
-        else
-        {
-            switch (element.name)
-            {
-                case "Air":
-                    transform.GetComponent<SpriteRenderer>().color = new Color(165, 176, 206, 1);
-                    break;
-                case "Fire":
-                    transform.GetComponent<SpriteRenderer>().color = new Color(148, 36, 0, 1);
-                    break;
-                case "Earth":
-                    transform.GetComponent<SpriteRenderer>().color = new Color(74, 43, 27, 1);
-                    break;
-            }
-        }
-        
-    }
+	void Update() {
+		if (!occupied) {
+			transform.GetComponent<SpriteRenderer>().color = Color.white;
+		} else {
+			switch (element.name) {
+				case "Air":
+					transform.GetComponent<SpriteRenderer>().color = airColor;
+					break;
+				case "Fire":
+					transform.GetComponent<SpriteRenderer>().color = fireColor;
+					break;
+				case "Earth":
+					transform.GetComponent<SpriteRenderer>().color = earthColor;
+					break;
+			}
+		}
+	}
 }
